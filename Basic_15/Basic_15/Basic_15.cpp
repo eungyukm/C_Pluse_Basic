5b534c69af97f8baa2698f3eada3d5350b69164e
eungyukm <eungyukm@gmail.com>
2025-04-02 17:21:38 +0900

    feat: upload template, lambda

---
+++ Basic_15/Basic_15/Basic_15.cpp
@@ -0,0 +1,26 @@
+﻿// Basic_15.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Box
+{
+    T value;
+
+public:
+    Box(T v) : value(v) {}
+    void print()
+    {
+        cout << "Value: " << value << endl;
+    }
+};
+int main()
+{
+    Box<int> intBox(10);
+    Box<string> strBox("Hello");
+
+    intBox.print();
+    strBox.print();
+}
---
+++ Basic_16/Basic_16/Basic_16.cpp
@@ -0,0 +1,19 @@
+﻿// Basic_16.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <iostream>
+using namespace std;
+
+// 람다 캡처 (Lambda Capture)
+// 람다 함수에서 외부 변수를 가져오는 방법입니다.
+// [] 안에 변수를 넣어야 외부 값을 사용할 수 있습니다.
+
+int main()
+{
+	int x = 5;
+	auto func = [x]()
+		{
+			cout << "x = " << x << endl;
+		};
+
+	func();
+}
