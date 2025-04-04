8fa0cf9da11ced4f7b85c3638e27366cc7cca2ba
eungyukm <eungyukm@gmail.com>
2025-04-04 23:46:33 +0900

    feat: upload mutex project

---
+++ Basic_22/Basic_22/Basic_22.cpp
@@ -0,0 +1,36 @@
+﻿// Basic_22.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+// mutex란?
+// 여러 스레드가 동시에 자원(변수 등)에 접근하지
+// 못하도록 막아주는 도구입니다.
+int counter = 0;
+// mutex 선언
+mutex m;
+
+void add()
+{
+	for (int i = 0; i < 10000; i++)
+	{
+		// 잠금
+		m.lock();
+		++counter;
+		// 잠금 해제
+		m.unlock();
+	}
+}
+int main()
+{
+	thread t1(add);
+	thread t2(add);
+
+	t1.join();
+	t2.join();
+
+	cout << "Couter: " << counter << endl;
+}
---
+++ Basic_23/Basic_23/Basic_23.cpp
@@ -0,0 +1,34 @@
+﻿// Basic_23.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+// 쓰레드 사용 시 lock()과 unlock() 대신
+// 간편한 lock_guard
+// RAll 방식으로 자동 잠금/해제 합니다.
+
+int counter = 0;
+mutex m;
+
+void add()
+{
+	for (int i = 0; i < 10000; i++)
+	{
+		// 블록을 벗어나면 자동으로 unlock
+		lock_guard<mutex> lock(m);
+		++counter;
+	}
+}
+int main()
+{
+	thread t1(add);
+	thread t2(add);
+
+	t1.join();
+	t2.join();
+
+	cout << "Counter: " << counter << endl;
+}
