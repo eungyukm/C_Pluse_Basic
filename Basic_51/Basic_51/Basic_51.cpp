aa72fdfe9addc88e5a2a197dcdc222dd5a32a396
eungyukm <eungyukm@gmail.com>
2025-04-25 19:50:21 +0900

    feat: upload game system code

---
+++ Basic_51/Basic_51/Basic_51.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand((unsigned)time(0));
+
+	int evasionChance = 30;
+	int damage = 30;
+
+	int roll = rand() % 100;
+	if (roll < evasionChance)
+	{
+		cout << "회피 성공! 피해 없음" <<
+			endl;
+	}
+	else
+	{
+		cout << "피격! " << damage << " 데미지"
+			<< endl;
+	}
+}
---
+++ Basic_52/Basic_52/Basic_52.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand((unsigned)time(0));
+
+	int upgradeChance = 40;
+	int level = 0;
+
+	int roll = rand() % 100;
+	if (roll < upgradeChance)
+	{
+		level++;
+		cout << " 강화 성공! + (레벨 :"
+			<< level << " )" << endl;
+	}
+	else
+	{
+		cout << " 강화 실패!" << endl;
+	}
+}
